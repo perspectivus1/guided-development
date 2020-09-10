bb1c099c2875217a9e328a4f5ac23a0ef70e175e
Eyal Barlev <eyal.barlev@sap.com>
2020-09-10 18:29:41 +0300

    Updated sample contributor to support dynamic items and collections

--- contrib-cake/src/extension.ts
+++ contrib-cake/src/extension.ts
@@ -4,13 +4,16 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as _ from 'lodash';
 
+
 const datauri = require("datauri");
 
 const EXT_ID = "saposs.contrib-cake";
+
 let changedCallback: (id: string) => void;
 let changedCallbackThis: Object;
-const bakeCollectionMap: Map<string, ICollection> = new Map();
-
+const bakeCollectionMap: Map<string, ICollection> = new Map(); // key is dirname; value is collection
+const bakeItemsMap: Map<string, Array<string>> = new Map(); // key is dirname; value is array of item ids
+let extensionPath: string;
 let bakeCollectionTemplate: ICollection;
 
 bakeCollectionTemplate = {
@@ -19,15 +22,37 @@ bakeCollectionTemplate = {
     description: "This is a repice for making baked cakes. You can bake a cake only if there is a bake.json file in your workspace",
     type: CollectionType.Scenario,
     itemIds: [
-        "saposs.contrib-cake.buy-ingredients",
-        "saposs.contrib-oven.prep-oven",
-        "saposs.contrib-cake.mix-ingredients",
-        "saposs.contrib-cake.pour-mix",
-        "saposs.contrib-cake.place-pan",
-        "saposs.contrib-oven.bake",
-        "saposs.contrib-cake.eat-cake",
+        `${EXT_ID}.buy-ingredients`,
+        `saposs.contrib-oven.prep-oven`,
+        `${EXT_ID}.mix-ingredients`,
+        `${EXT_ID}.pour-mix`,
+        `${EXT_ID}.place-pan`,
+        `saposs.contrib-oven.bake`,
+        `${EXT_ID}.eat-cake`,
     ]
 };
+
+function replacer(key: any, value: any) {
+    if (typeof value === 'function') {
+        value = "__Function";       
+    }
+    return value;
+}
+
+function cloneItems(items: Array<IItem>): Array<IItem> {
+    const clonedItems: Array<IItem> = JSON.parse(JSON.stringify(items, replacer));
+    for (const item of clonedItems) {
+        const origItem: IItem | undefined = items.find(value => value.id === item.id);
+        for (const prop in item.action) {
+            if ((item.action as any)[prop] === "__Function") {
+                if (origItem) {
+                    (item.action as any)[prop] = ((origItem.action as any)[prop] as Function).bind(item);
+                }
+            }
+        }
+    }
+    return clonedItems;
+}
 
 function getCollections(): ICollection[] {
     const collections: ICollection[] = [];
@@ -39,10 +64,10 @@ function getCollections(): ICollection[] {
         description: "This is a recipe for making a no-bake cake",
         type: CollectionType.Scenario,
         itemIds: [
-            "saposs.contrib-cake.buy-ingredients",
-            "saposs.contrib-cake.mix-ingredients",
-            "saposs.contrib-cake.pour-mix",
-            "saposs.contrib-cake.eat-cake",
+            `${EXT_ID}.buy-ingredients`,
+            `${EXT_ID}.mix-ingredients`,
+            `${EXT_ID}.pour-mix`,
+            `${EXT_ID}.eat-cake`,
         ]
     };
     collections.push(collection);
@@ -54,26 +79,161 @@ function getCollections(): ICollection[] {
     return collections;
 }
 
-function addBakeCollection(path: string): void {
-    // clone collection template
-    const collection: ICollection = JSON.parse(JSON.stringify(bakeCollectionTemplate));
-    collection.id = `bake-${path}`;
-    collection.title = `Bake a Cake (${path})`;
-    bakeCollectionMap.set(path, collection);
+function getItems(): Array<IItem> {
+    const initialItems: Array<IItem> = getInitialItems();
+    const items: Array<IItem> = [];
+
+    for (const mapEntry of bakeItemsMap) {
+        for (const itemId of mapEntry[1]) {
+            const origItem = initialItems.find(value => itemId.includes(`${EXT_ID}.${value.id}`));
+            if (origItem) {
+                const clonedItem: IItem = cloneItems([origItem])[0];
+                clonedItem.id = `${origItem.id}-${mapEntry[0]}`;
+                items.push(clonedItem);
+            }
+        }
+    }
+
+    items.push(...initialItems);
+    return items;
 }
 
-function removeBakeCollection(path: string): void {
-    bakeCollectionMap.delete(path);
+function getInitialItems(): Array<IItem> {
+    const items: Array<IItem> = [];
+    let item: IItem = {
+        id: "eat-cake",
+        title: "Eat Cake",
+        description: "Bon appetite",
+        image: getImage(path.join(extensionPath, 'resources', 'cake1.jpg')),
+        action: {
+            name: "Eat",
+            type: ActionType.Execute,
+            performAction: () => {
+                return vscode.commands.executeCommand("workbench.action.openGlobalSettings");
+            },
+        },
+        labels: [
+            { "Project Name": "cap1" },
+            { "Project Type": "CAP" },
+            { "Path": "/home/user/projects/cap1" }
+        ]
+    };
+    items.push(item);
+
+    item = {
+        id: "buy-ingredients",
+        title: "Buy Ingredients",
+        description: "Buy relevant ingredeients for your cake",
+        action: {
+            name: "Buy",
+            type: ActionType.Execute,
+            performAction: () => {
+                return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
+            },
+        },
+        labels: []
+    };
+    items.push(item);
+
+    item = {
+        id: "mix-ingredients",
+        title: "Mix Ingredients",
+        description: "Mix ingredeients according to recipe",
+        action: {
+            name: "Mix",
+            type: ActionType.Execute,
+            performAction: () => {
+                return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
+            },
+        },
+        labels: []
+    };
+    items.push(item);
+
+    item = {
+        id: "insert-pan",
+        title: "Insert Pan into Oven",
+        description: "Insert the pan into the oven",
+        action: {
+            name: "Insert",
+            type: ActionType.Execute,
+            performAction: () => {
+                return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
+            },
+        },
+        labels: []
+    };
+    items.push(item);
+
+    item = {
+        id: "pour-mix",
+        title: "Pour Mix into Pan",
+        description: "Pour the cake mix into the pan",
+        image: getImage(path.join(extensionPath, 'resources', 'info.png')),
+        action: {
+            name: "Pour",
+            type: ActionType.Execute,
+            performAction: () => {
+                return vscode.window.showInformationMessage("The cake mix was poured into the pan");
+            },
+        },
+        labels: [
+            { "Project Name": "cap2" },
+            { "Project Type": "CAP" },
+            { "Path": "/home/user/projects/cap2" }
+        ]
+    };
+    items.push(item);
+
+    item = {
+        id: "place-pan",
+        title: "Place Pan in Oven",
+        description: "Place the pan with the mix in the oven",
+        itemIds: [
+            "saposs.contrib-oven.open-oven",
+            `${EXT_ID}.insert-pan`,
+            "saposs.contrib-oven.close-oven"
+        ],
+        labels: [
+            { "Project Name": "cap2" },
+            { "Project Type": "CAP" },
+            { "Path": "/home/user/projects/cap2" }
+        ]
+    };
+    items.push(item);
+
+    return items;
+}
+
+function addBakeCollection(dirPath: string): void {
+    const name = path.parse(dirPath).name;
+
+    // clone collection template
+    const collection: ICollection = JSON.parse(JSON.stringify(bakeCollectionTemplate));
+    collection.id = `bake-${name}`;
+    collection.title = `Bake a Cake (${name})`;
+    for (const index in collection.itemIds) {
+        collection.itemIds[index] = `${collection.itemIds[index]}-${name}`;
+    }
+    bakeCollectionMap.set(name, collection);
+    bakeItemsMap.set(name, collection.itemIds);
+}
+
+function removeBakeCollection(dirPath: string): void {
+    const name = path.parse(dirPath).name;
+    bakeCollectionMap.delete(name);
+    bakeItemsMap.delete(name);
 }
 
 export function activate(context: vscode.ExtensionContext) {
+    extensionPath = context.extensionPath;
     console.log('Congratulations, your extension "contrib-cake" is now active!');
 
     vscode.workspace.onDidChangeWorkspaceFolders((e) => {
         // when first folder is added to the workspace, the extension is reactivated, so we could let the find files upon activation handle this use-case
         // when last folder removed from workspace, the extension is reactivated, so we could let the find files upon activation handle this use-case
 
-        for (const folder of e.removed) { 
+        for (const folder of e.removed) {
             console.dir(`${folder.uri.path} removed from workspace`);
             removeBakeCollection(folder.uri.path);
             if (changedCallback) {
@@ -81,7 +241,7 @@ export function activate(context: vscode.ExtensionContext) {
             }
         }
 
-        for (const folder of e.added) { 
+        for (const folder of e.added) {
             console.dir(`${folder.uri.path} added to workspace`);
             addBakeCollection(folder.uri.path);
             if (changedCallback) {
@@ -131,110 +291,9 @@ export function activate(context: vscode.ExtensionContext) {
             return getCollections();
         },
         getItems: () => {
-            const items: Array<IItem> = [];
-            let item: IItem = {
-                id: "eat-cake",
-                title: "Eat Cake",
-                description: "Bon appetite",
-                image: getImage(path.join(context.extensionPath, 'resources', 'cake1.jpg')),
-                action: {
-                    name: "Eat",
-                    type: ActionType.Execute,
-                    performAction: () => {
-                        return vscode.commands.executeCommand("workbench.action.openGlobalSettings");
-                    },
-                },
-                labels: [
-                    { "Project Name": "cap1" },
-                    { "Project Type": "CAP" },
-                    { "Path": "/home/user/projects/cap1" }
-                ]
-            };
-            items.push(item);
-
-            item = {
-                id: "buy-ingredients",
-                title: "Buy Ingredients",
-                description: "Buy relevant ingredeients for your cake",
-                action: {
-                    name: "Buy",
-                    type: ActionType.Execute,
-                    performAction: () => {
-                        return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
-                    },
-                },
-                labels: []
-            };
-            items.push(item);
-
-            item = {
-                id: "mix-ingredients",
-                title: "Mix Ingredients",
-                description: "Mix ingredeients according to recipe",
-                action: {
-                    name: "Mix",
-                    type: ActionType.Execute,
-                    performAction: () => {
-                        return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
-                    },
-                },
-                labels: []
-            };
-            items.push(item);
-
-            item = {
-                id: "insert-pan",
-                title: "Insert Pan into Oven",
-                description: "Insert the pan into the oven",
-                action: {
-                    name: "Insert",
-                    type: ActionType.Execute,
-                    performAction: () => {
-                        return vscode.commands.executeCommand("git.clone", "https://github.com/SAP/code-snippet.git");
-                    },
-                },
-                labels: []
-            };
-            items.push(item);
-
-            item = {
-                id: "pour-mix",
-                title: "Pour Mix into Pan",
-                description: "Pour the cake mix into the pan",
-                image: getImage(path.join(context.extensionPath, 'resources', 'info.png')),
-                action: {
-                    name: "Pour",
-                    type: ActionType.Execute,
-                    performAction: () => {
-                        return vscode.window.showInformationMessage("The cake mix was poured into the pan");
-                    },
-                },
-                labels: [
-                    { "Project Name": "cap2" },
-                    { "Project Type": "CAP" },
-                    { "Path": "/home/user/projects/cap2" }
-                ]
-            };
-            items.push(item);
-
-            item = {
-                id: "place-pan",
-                title: "Place Pan in Oven",
-                description: "Place the pan with the mix in the oven",
-                itemIds: [
-                    "saposs.contrib-oven.open-oven",
-                    "saposs.contrib-cake.insert-pan",
-                    "saposs.contrib-oven.close-oven"
-                ],
-                labels: [
-                    { "Project Name": "cap2" },
-                    { "Project Type": "CAP" },
-                    { "Path": "/home/user/projects/cap2" }
-                ]
-            };
-            items.push(item);
-
-            return items;
+            const items = getItems();
+            const clonedItems = cloneItems(items);
+            return clonedItems;
         },
         registerOnChangedCallback: (thisArg: Object, callback: (id: string) => void) => {
             changedCallbackThis = thisArg;
